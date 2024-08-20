4f42bf5e4e653ced74f80bca013b4080eafd5b87
cliffordmarkmusni <cliffordmarkmusni@gmail.com>
2024-08-21 04:12:20 +0800

    Added .env file for base URL

--- src/store/actions/category-actions.js
+++ src/store/actions/category-actions.js
@@ -1,11 +1,12 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
-import { baseUrl } from "../../api";
 
 export const fetchCategories = createAsyncThunk(
   "products/fetchCategories",
   async () => {
-    const response = await axios.get(`${baseUrl}/categories`);
+    const response = await axios.get(
+      `${process.env.REACT_APP_PRINTFUL_BASE_URL}/categories`
+    );
     return response.data?.result?.categories;
   }
 );
--- src/store/actions/product-actions.js
+++ src/store/actions/product-actions.js
@@ -1,12 +1,13 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
-import { baseUrl } from "../../api";
 
 export const fetchProducts = createAsyncThunk(
   "products/fetchProducts",
   async ({ category_id }, { rejectWithValue }) => {
     const response = await axios.get(
-      `${baseUrl}/products${category_id ? "?category_id=" + category_id : ""}`
+      `${process.env.REACT_APP_PRINTFUL_BASE_URL}/products${
+        category_id ? "?category_id=" + category_id : ""
+      }`
     );
     return response.data?.result;
   }
